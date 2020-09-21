5e3f800ceaf10dc387a076e44be903a39cc5cf9f
kdlry <kristen.delorey@live.com>
2020-09-21 18:23:23 -0400

    working jsgit add -A

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// ** Name space ** 
+// Name spaces
 const spaceApp = {};
 
-// ** API properties **
+// API properties + global variables
 
 spaceApp.onWaterKey = 'EkuV5rQoh2edgP-RgG-B';
 spaceApp.mapquestKey = 'rNUBvav2dEGGss4WVvHK64tVGGygn3zB';
@@ -10,12 +10,12 @@ spaceApp.issCoordEndPoint = 'https://api.wheretheiss.at/v1/satellites/';
 spaceApp.landOrWaterEndPoint = 'https://api.onwater.io/api/v1/results/';
 spaceApp.locationEndPoint = 'https://www.mapquestapi.com/geocoding/v1/address';
 
-spaceApp.latitude = 0;
-spaceApp.longitude = 0;
+spaceApp.latitude = undefined;
+spaceApp.longitude = undefined;
 spaceApp.userLat = 0;
 spaceApp.userLng = 0;
 
-// ** API calls **
+// API call
 spaceApp.findStation = () => {
    $.ajax({
       url: spaceApp.issCoordEndPoint,
@@ -25,8 +25,8 @@ spaceApp.findStation = () => {
          id: 25544
       },
    }).then((res) => {
-      console.log(res);
-      console.log('I work')
+      // console.log(res);
+      // console.log('I work')
       spaceApp.satDetails(res);
 
       spaceApp.latitude = (res.latitude);
@@ -34,12 +34,12 @@ spaceApp.findStation = () => {
 
       spaceApp.locateStation();
 
-      console.log(spaceApp.latitude);
-      console.log(spaceApp.longitude);
+      // console.log(spaceApp.latitude);
+      // console.log(spaceApp.longitude);
    });
 }
 
-//Function that displays satellite info on the page
+// Function that displays ISS position info on page
 spaceApp.satDetails = (res) => {
    console.log(res);
 
@@ -73,13 +73,13 @@ spaceApp.satDetails = (res) => {
       $('#satDisplay2').append(info2);
       
       // Latitiude
-      const lat = $('<p>').text(res.latitude);
+      const lat = $('<p>').text(res.latitude.toFixed(6));
       const info3 = $('.info3').html(lat);
 
       $('#satDisplay3').append(info3);
       
       // Longitude
-      const long = $('<p>').text(res.longitude);
+      const long = $('<p>').text(res.longitude.toFixed(6));
       const info4 = $('.info4').html(long);
 
       $('#satDisplay4').append(info4);
@@ -91,7 +91,7 @@ spaceApp.satDetails = (res) => {
       $('#satDisplay5').append(info5);
       
       // Velocity
-      const veloc = $('<p>').text(res.velocity);
+      const veloc = $('<p>').text(res.velocity.toFixed(3));
       const info6 = $('.info6').html(veloc);
 
       $('#satDisplay6').append(info6);
@@ -106,11 +106,11 @@ spaceApp.locateStation = () => {
          access_token: spaceApp.onWaterKey,
       }
    }).then((res) => {
-      console.log(res);
-      console.log('I work alsooooo')
+      // console.log(res);
+      // console.log('I work alsooooo')
       spaceApp.satLocation(res)
    }).fail((err) => {
-      console.log(err);
+      // console.log(err);
    });
 }
 
@@ -145,21 +145,22 @@ spaceApp.searchLocation = (location) => {
          location: location,
       },
    }).then((res) => {
-      console.log(res.results[0].locations[0].latLng.lat);
-      console.log(res.results[0].locations[0].latLng.lng);
 
       spaceApp.userLat = (res.results[0].locations[0].latLng.lng);
       spaceApp.userLong = (res.results[0].locations[0].latLng.lat);
 
-      console.log(getDistanceFromLatLonInKm);
+      const distance = spaceApp.calculateLocation(spaceApp.latitude, spaceApp.longitude, spaceApp.userLat, spaceApp.userLng);
+      const finalDistance = distance.toFixed(0);
+
+      $('.distanceResults').html(`<p>Your location is ${finalDistance} km from the space station.</p>`).addClass('displayTxt');
+      
    });
 }
 
-// Function that looks for user input on form submit
 spaceApp.show = function() {
    $('.satButton').on('click', function(e) {
       e.preventDefault();
-      console.log('I work too')
+      // console.log('I work too')
 
       spaceApp.findStation();
       
@@ -170,12 +171,16 @@ spaceApp.show = function() {
 spaceApp.reset = function () {
    $('.resetButton').on('click', function() {
       
-      // Remove satellite info
+      // Remove ISS info
       $('.info').find('p').addClass('hide');
 
       // Remove land/water info
       $('.landOrWater').find('p').addClass('hide');
       $('.landOrWater').find('img').addClass('hide');
+
+      //Remove location input + distance text
+      $('input').val('');
+      $('.distanceResults').find('p').addClass('hide');
    });
 }
 
@@ -184,37 +189,41 @@ spaceApp.findLocation = function() {
       e.preventDefault();
       console.log('Submit working');
 
-      // Start location input
-      let userLocationInput = $('input').val();
-      console.log(userLocationInput);
-
-      // spaceApp.searchLocation(userLocationInput);
-
-      // console.log(spaceApp.calculateLocation);
+      if (spaceApp.latitude === undefined && spaceApp.longitude === undefined) {
+         alert('Sorry, you need to find the station position first!');
+         $('input').val('');
+      } else {
+         // Start location input
+         let userLocationInput = $('input').val();
+         console.log(userLocationInput);
+   
+         spaceApp.searchLocation(userLocationInput);
+   
+         // console.log(spaceApp.calculateLocation);
+      }
    });
 }
 
 // https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
-// spaceApp.calculateLocation = function() {
-//    function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
-//       let R = 6371; // Radius of the earth in km
-//       let dLat = deg2rad(lat2 - lat1);  // deg2rad below
-//       let dLon = deg2rad(lon2 - lon1);
-//       let a =
-//          Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-//          Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-//          Math.sin(dLon / 2) * Math.sin(dLon / 2)
-//          ;
-//       let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-//       let d = R * c; // Distance in km
-//       return d;
-//    }
 
-//    function deg2rad(deg) {
-//       return deg * (Math.PI / 180)
-//    }
-//    spaceApp.calculateLocation(spaceApp.latitude, spaceApp.longitude, spaceApp.userLat, spaceApp.userLng)
-// }
+spaceApp.calculateLocation = function(lat1, lon1, lat2, lon2) {
+   let R = 6371; // Radius of the earth in km
+   let dLat = spaceApp.deg2rad(lat2 - lat1);  // spaceApp.deg2rad below
+   let dLon = spaceApp.deg2rad(lon2 - lon1);
+   let a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(spaceApp.deg2rad(lat1)) * Math.cos(spaceApp.deg2rad(lat2)) *
+      Math.sin(dLon / 2) * Math.sin(dLon / 2)
+      ;
+   let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+   let d = R * c; // Distance in km
+   return d;
+}
+
+spaceApp.deg2rad = function(deg) {
+   return deg * (Math.PI / 180)
+}
+   
 
 // Initializing functions
 spaceApp.init = function() {
@@ -226,5 +235,4 @@ spaceApp.init = function() {
 // Document ready
 $(function () {
    spaceApp.init();
-   // alert("Welcome To Code Break, Time to Rest Those Eyes & Get Outside! ");
 });
